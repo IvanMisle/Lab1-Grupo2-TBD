aa2c855c460b582153110f72d6cd2160938b1c6f
Juan Pablo Cardenas <juan.cardenas.r@usach.cl>
2024-04-11 17:57:52 -0400

    Carpeta con Spring

---
+++ Lab1_TBD/src/test/java/Lab1_TBD/Lab1TbdApplicationTests.java
@@ -0,0 +1,13 @@
+package Lab1_TBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1TbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
